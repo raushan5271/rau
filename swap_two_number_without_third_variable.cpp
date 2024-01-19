9734621358f41eb725ff04e8902f9b3249e2829d
raushan5271 <19110110041+raushan5271@users.noreply.github.com>
2024-01-19 20:42:46 +0530

    Create swap_two_number_without_third_variable.cpp

---
+++ swap_two_number_without_third_variable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter number ";
+    cin>>a>>b;
+    cout<<"a="<<a<<"b="<<b<<endl;
+    cout<<"Before swap number is"<<a<<b<<endl;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"after swap number is "<<a<<b;
+    return 0;
+}
