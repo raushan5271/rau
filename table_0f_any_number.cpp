5a0108fed2f4b425744df730cd997ed5d0762b1b
raushan5271 <19110110041+raushan5271@users.noreply.github.com>
2024-01-19 21:15:54 +0530

    Create table_0f_any_number.cpp

---
+++ table_0f_any_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i = 1; i <= 10; i++)
+    {
+        i*n;
+        cout<<i<<"*"<<n<<"=" <<i*n<<endl;
+    }
+    
+}
